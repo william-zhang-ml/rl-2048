d521c97e4eb9398c4db739fc3d68424496468836
william-zhang-ml <william.zhang.ml@gmail.com>
2021-09-29 03:22:57 -0400

    added 2048 game interface

---
+++ rl_2048/__init__.py
@@ -0,0 +1,33 @@
+""" Tools for reinforcement learning applied to 2048. """
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+class Game2048(Chrome):
+    """ Interface for a reinforcement learning agent to play 2048. """
+    def __init__(self,
+                 path: str,
+                 headless: bool = False) -> None:
+        """ Constructor.
+
+        :param path:     path to chromedriver
+        :type  path:     str
+        :param headless: run browser headless or not, defaults to False
+        :type  headless: bool, optional
+        """
+        options = Options()
+        if headless:
+            options.add_argument('--headless')
+        super(Game2048, self).__init__(path, options=options)
+        self.actions = [
+            ActionChains(self).send_keys(Keys.UP),
+            ActionChains(self).send_keys(Keys.LEFT),
+            ActionChains(self).send_keys(Keys.DOWN),
+            ActionChains(self).send_keys(Keys.RIGHT)]
+
+    def __enter__(self) -> None:
+        """ Open browser and game. """
+        super(Game2048, self).__enter__()
+        self.get('https://2048game.com/')

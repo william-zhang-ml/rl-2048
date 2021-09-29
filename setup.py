a687961aa49e10de580bf4a161cdc700a8ef6459
william-zhang-ml <william.zhang.ml@gmail.com>
2021-09-29 03:15:13 -0400

    added setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='rl-2048',
+    version='0.0.1',
+    url="https://github.com/william-zhang-ml/rl-2048",
+    author="William Zhang",
+    author_email="william.zhang.ml@gmail.com",
+    packages=['rl_2048'],
+    install_requires=[
+        'torch>=1.8.1',
+        'numpy>=1.18.5'
+    ]
+)
